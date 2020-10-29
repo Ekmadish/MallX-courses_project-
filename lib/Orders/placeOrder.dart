c01c0be39c6c9c0e489003ccc40d86b4f2b65e98
Ekmadish <estaykargabay1206@gmail.com>
2020-10-29 12:26:17 +0600

    orders func

--- lib/Orders/placeOrder.dart
+++ lib/Orders/placeOrder.dart
@@ -1,5 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:malX/Config/config.dart';
+import 'package:malX/Counters/cartitemcounter.dart';
+import 'package:malX/Store/storehome.dart';
 import 'package:provider/provider.dart';
 
 class PaymentPage extends StatefulWidget {
@@ -17,5 +21,85 @@ class _PaymentPageState extends State<PaymentPage> {
   @override
   Widget build(BuildContext context) {
     return Material();
+  }
+
+  addOrderDetails() {
+    orderdetaiLsForUsers({
+      EcommerceApp.addressID: widget.addressId,
+      EcommerceApp.totalAmount: widget.totalAmount,
+      "orderBy": EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID),
+      EcommerceApp.productID: EcommerceApp.sharedPreferences
+          .getStringList(EcommerceApp.userCartList),
+      EcommerceApp.paymentDetails: "Cash on Delivery",
+      EcommerceApp.orderTime: DateTime.now().millisecondsSinceEpoch.toString(),
+      EcommerceApp.isSuccess: true,
+    });
+
+    orderdetaiLsForAdmin({
+      EcommerceApp.addressID: widget.addressId,
+      EcommerceApp.totalAmount: widget.totalAmount,
+      "orderBy": EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID),
+      EcommerceApp.productID: EcommerceApp.sharedPreferences
+          .getStringList(EcommerceApp.userCartList),
+      EcommerceApp.paymentDetails: "Cash on Delivery",
+      EcommerceApp.orderTime: DateTime.now().millisecondsSinceEpoch.toString(),
+      EcommerceApp.isSuccess: true,
+    }).whenComplete(() => {
+          emptyCardNow(),
+        });
+  }
+
+  emptyCardNow() async {
+    await EcommerceApp.sharedPreferences
+        .setStringList(EcommerceApp.userCartList, ["garbageValue"]);
+
+    List tepmList =
+        EcommerceApp.sharedPreferences.getStringList(EcommerceApp.userCartList);
+
+    Firestore.instance
+        .collection("users")
+        .document(
+            EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID))
+        .updateData({
+      EcommerceApp.userCartList: tepmList,
+    }).then((value) {
+      EcommerceApp.sharedPreferences
+          .setStringList(EcommerceApp.userCartList, tepmList);
+      Provider.of<CartItemCounter>(context, listen: false).displayResult();
+    });
+
+    SnackBar(
+      backgroundColor: Colors.black,
+      content: Text(
+        "Youre Order has been placed successfully.",
+        style: TextStyle(color: Colors.lightGreen),
+      ),
+    );
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => StoreHome()),
+    );
+    // Fluttertoast.showToast(msg: "");
+  }
+
+  Future orderdetaiLsForUsers(Map<String, dynamic> data) async {
+    await EcommerceApp.firestore
+        .collection(EcommerceApp.collectionUser)
+        .document(
+            EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID))
+        .collection(EcommerceApp.collectionOrders)
+        .document(
+            EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID) +
+                data['orderTime'])
+        .setData(data);
+  }
+
+  Future orderdetaiLsForAdmin(Map<String, dynamic> data) async {
+    await EcommerceApp.firestore
+        .collection(EcommerceApp.collectionOrders)
+        .document(
+            EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID) +
+                data['orderTime'])
+        .setData(data);
   }
 }
