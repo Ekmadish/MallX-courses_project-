fe6c9485c1a162446a767bec739bd7e2b0cef459
Ekmadish <estaykargabay1206@gmail.com>
2020-11-02 18:17:36 +0600

    fix bug

--- lib/Orders/OrderDetailsPage.dart
+++ lib/Orders/OrderDetailsPage.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:malX/Address/address.dart';
