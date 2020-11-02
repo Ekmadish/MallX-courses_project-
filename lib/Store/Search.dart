a878a87910117657ec853fcf58623da0d9fc1b07
Ekmadish <estaykargabay1206@gmail.com>
2020-11-02 20:21:27 +0600

    search function added

--- lib/Store/Search.dart
+++ lib/Store/Search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:malX/Models/item.dart';
+import 'package:malX/Store/storehome.dart';
 import 'package:malX/main.dart';
 import '../Widgets/customAppBar.dart';
 
@@ -9,6 +11,8 @@ class SearchProduct extends StatefulWidget {
 }
 
 class _SearchProductState extends State<SearchProduct> {
+  Future<QuerySnapshot> docList;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,13 +23,76 @@ class _SearchProductState extends State<SearchProduct> {
             preferredSize: Size(56, 56),
           ),
         ),
+        body: FutureBuilder<QuerySnapshot>(
+          builder: (context, snap) {
+            return snap.hasData
+                ? ListView.builder(
+                    itemCount: snap.data.documents.length,
+                    itemBuilder: (context, index) {
+                      ItemModel itemModel =
+                          ItemModel.fromJson(snap.data.documents[index].data);
+                      return sourceInfo(itemModel, context);
+                    },
+                  )
+                : Text("No data ...");
+          },
+          future: docList,
+        ),
       ),
     );
   }
 
-  searchWidget() {
+  Widget searchWidget() {
+    return Container(
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width,
+      height: 80,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.grey, Colors.green],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(0.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 8),
+              child: Icon(
+                Icons.search,
+                color: Colors.green,
+              ),
+            ),
+            Flexible(
+              child: Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: TextField(
+                  onChanged: (value) {
+                    startSearching(value);
+                  },
+                  decoration: InputDecoration.collapsed(hintText: "Search ..."),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-    return 
+  Future startSearching(String query) async {
+    docList = Firestore.instance
+        .collection("items")
+        .where("shortinfo", isGreaterThanOrEqualTo: query)
+        .getDocuments();
   }
 }
 
